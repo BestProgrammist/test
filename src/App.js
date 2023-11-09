6838cebf5dac98463a73a766381d623cabb3dbdf
Rustam <rustamisomiddinovh77@gmail.com>
2023-11-09 17:27:25 +0300

    ghjk

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import Loginpage from './pages/Loginpage.js';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        <Loginpage />
-      {/* </header> */}
+      <Loginpage />
     </div>
   );
 }
--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,10 +1,32 @@
 import React from "react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link} from "@nextui-org/react";
+import "./pages.css";
 import { Backpage } from "./Backpage";
 
 function Loginpage(params) {
     return (
         <Backpage>
-            hkjkj
+            <Card className="card">
+                <CardHeader className="flex gap-3">
+                    <div className="flex flex-col">
+                        <p className="text-md">Sign up</p>
+                    </div>
+                </CardHeader>
+                {/* <Divider/> */}
+                <CardBody>
+                    <p>Make beautiful websites regardless of your design experience.</p>
+                </CardBody>
+                {/* <Divider/> */}
+                <CardFooter>
+                    <Link
+                    isExternal
+                    showAnchorIcon
+                    href="https://github.com/nextui-org/nextui"
+                    >
+                    Visit source code on GitHub.
+                    </Link>
+                </CardFooter>
+            </Card>
         </Backpage>
     )
 }
